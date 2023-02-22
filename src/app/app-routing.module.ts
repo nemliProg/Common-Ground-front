e0a1b437b0ebdd1f8c039d4c2c5f99bf01165cb5
nemliProg <younesbuzz@gmail.com>
2023-02-22 21:40:37 +0100

    some changes not related with folder structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './shared/components/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/fefefef',
+    pathMatch : 'full'
+  },
+  { path: 'public', loadChildren: () => import('./public/public.module').then(m => m.PublicModule) },
+  { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
+  { path: 'member', loadChildren: () => import('./member/member.module').then(m => m.MemberModule) },
+  { path: 'agent', loadChildren: () => import('./agent/agent.module').then(m => m.AgentModule) },
+  {
+    path: '**',
+    component: NotFoundComponent
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: []
 })
 export class AppRoutingModule { }
